4b56a204418504cb2f0b1878ff80c83247e70e76
jigkoxsee <ked.naja@gmail.com>
2015-03-18 01:14:31 +0700

    Add route file

--- main.go
+++ main.go
@@ -1,82 +1,19 @@
 package main
 
 import (
-	"./model"
 	"flag"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	"github.com/yosssi/ace"
 	"log"
 	"net/http"
 )
 
-func AddBookHandler(w http.ResponseWriter, r *http.Request) {
-	tpl, err := ace.Load("view/base", "view/book/add", &ace.Options{DynamicReload: true})
-	if err != nil {
-		panic(err)
-	}
-
-	if err := tpl.Execute(w, nil); err != nil {
-		panic(err)
-	}
-}
-func RootHandler(w http.ResponseWriter, r *http.Request) {
-	//response.Header().Set("Content-type", "text/html")
-	//fmt.Fprintf(response, "Hey there!\n")
-
-	//--------ORM------
-	db, err := gorm.Open("mysql", "root:web@tcp(192.168.56.102:3306)/leafbox?charset=utf8&parseTime=True&loc=Local")
-
-	if err != nil {
-		panic(err)
-		return
-	}
-
-	var retData struct {
-		Items []model.Book
-	}
-
-	db.Order("id desc").Limit(5).Find(&retData.Items)
-
-	//log.Println("All rows:")
-	/*	for x, p := range retData.Items {
-			log.Printf("    %d: %v\n", x, p.Title)
-		}
-	*/
-
-	//------TPL-------
-
-	data := map[string]interface{}{
-		"Msg": []string{
-			"Hello",
-			"Ace",
-		},
-		"Item": retData.Items,
-	}
-
-	tpl, err := ace.Load("view/base", "view/home", &ace.Options{DynamicReload: true})
-	if err != nil {
-		panic(err)
-	}
-
-	if err := tpl.Execute(w, data); err != nil {
-		panic(err)
-	}
-}
-
 func main() {
 	var host = flag.String("host", "127.0.0.1", "IP of host to run webserver on")
 	var port = flag.Int("port", 8080, "Port to run webserver on")
-	var staticPath = flag.String("staticPath", "public/", "Path to static files")
 
 	flag.Parse()
 	fmt.Println("Start")
-	router := mux.NewRouter()
-	router.HandleFunc("/", RootHandler)
-	router.HandleFunc("/add", AddBookHandler)
-	router.PathPrefix("/").Handler(http.StripPrefix("", http.FileServer(http.Dir(*staticPath))))
+	router := GetRoute()
 
 	addr := fmt.Sprintf("%s:%d", *host, *port)
 	log.Printf("Listening on %s", addr)
---
+++ route.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"./model"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/yosssi/ace"
+	"net/http"
+)
+
+func AddBookHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := ace.Load("view/base", "view/book/add", &ace.Options{DynamicReload: true})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tpl.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	//--------ORM------
+	db, err := gorm.Open("mysql", "root:web@tcp(192.168.56.102:3306)/leafbox?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	var retData struct {
+		Items []model.Book
+	}
+
+	db.Order("id desc").Limit(5).Find(&retData.Items)
+	//------TPL-------
+	data := map[string]interface{}{
+		"Msg": []string{
+			"Hello",
+			"Ace",
+		},
+		"Item": retData.Items,
+	}
+
+	tpl, err := ace.Load("view/base", "view/home", &ace.Options{DynamicReload: true})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tpl.Execute(w, data); err != nil {
+		panic(err)
+	}
+}
+
+func GetRoute() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", RootHandler)
+	r.HandleFunc("/add", AddBookHandler)
+	r.PathPrefix("/").Handler(
+		http.StripPrefix("", http.FileServer(http.Dir("public/"))))
+	return r
+}
